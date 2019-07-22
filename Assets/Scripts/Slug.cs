24e512ed9129ae74de025f83b0ec4f0c34eb2f8b
thanhthao3243 <thanhthao3243@gmail.com>
2019-07-23 01:27:34 +0700

    enemies

---
+++ Assets/Scripts/Slug.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slug : MonoBehaviour
+{
+    public float speed;
+    private bool movingLeft = true;
+    public Transform groundDetection;
+
+    private void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, 2f);
+
+        if(groundInfo.collider == false)
+        {
+            if(movingLeft == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingLeft = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingLeft = true;
+            }
+        }
+    }
+}
